d579626c174e74e9b1ee870cf29639a2b85f5828
Rocco Angelella <rocchissimo936@gmail.com>
2025-10-29 16:44:24 +0100

    update server

--- server.py
+++ server.py
@@ -1,128 +1,16 @@
 from fastmcp import FastMCP
-import os
-import hashlib
-import pymupdf
-from sentence_transformers import SentenceTransformer
-from utils.mongo import _mongo_client
-from numpy import dot
-from numpy.linalg import norm
-import zlib
-import base64
-import textract
+
 
 mcp=FastMCP(
     name='My MCP Server'
 )
 
-def chunk_text(file_path,chunk_size,overlap):
-    try:
-        text=textract.process(file_path).decode('utf-8')
-    except Exception as e:
-        print(f"Error extracting text from {file_path}: {e}")
-        text = ""
-    chunks=[]
-    start=0
-    while start<len(text):
-        chunks.append(text[start:start+chunk_size])
-        start+=(chunk_size-overlap)
-    print('Text Chunked\n---------')
-    return chunks
-
-def embed_and_upload_file(chunks:list,embeddings_coll):
-    """ A function that returns the content of one or more PDF files as a string, given the folder specified by the user. """    
-    model=SentenceTransformer('all-MiniLM-L6-v2')
 
 
-
-    docs=[]
-    ids=[hashlib.sha256(chunk.encode('utf-8')).hexdigest() for chunk in chunks]
-    existing_ids=[item['_id'] for item in embeddings_coll.find({'_id':{'$in':ids}},{'_id':0,'chunk_id':1})]
-    print('Ids Ok, for loop deciding which to upload')
-    for id,chunk in zip(ids,chunks):
-        if id not in existing_ids:
-            chunk_bytes=chunk.encode('utf-8')
-            compressed=zlib.compress(chunk_bytes)
-            encoded=base64.b64encode(compressed).decode('ascii')
-            
-            embedding = model.encode(chunk)
-            try:
-                redundant_search=next(embeddings_coll.aggregate([
-                    {"$vectorSearch":{
-                        'index':'vector_index',
-                        'path':'embedding',
-                        'queryVector':embedding.tolist(),
-                        'numCandidates':10,
-                        'limit':1
-                    }}
-                ]))
-            except StopIteration:
-                redundant_search=None
-            if not redundant_search or dot(embedding,redundant_search['embedding'])/(norm(embedding)*norm(redundant_search['embedding']))<=0.93:
-                print('Cosine similarity lower than 0.93, appending.')
-                docs.append({'chunk_encoded':encoded,'_id':id,'embedding':embedding.tolist()})
-            else:
-                print('Very high cosine similarity, rdundant chunk')
-    if docs:
-        embeddings_coll.insert_many(docs)
-        print('New Embeddings uploaded to mongo')
-    else:
-        print('No new embeddings')
-
-def embed_prompt_and_retrieve(prompt:str)->str:
-    model=SentenceTransformer('all-MiniLM-L6-v2')
-    prompt_embedding=model.encode(prompt).tolist()
-    client=_mongo_client()
-    db=client['vector-db']
-    embeddings_coll=db['embeddings']
-    vector_search=[
-        {"$vectorSearch":{
-            'index':'vector_index',
-            'path':'embedding',
-            'queryVector':prompt_embedding,
-            'numCandidates':10,
-            'limit':5
-
-        }}
-    ]
-    result=list(embeddings_coll.aggregate(vector_search))
-    client.close()
-    return result
-
-def scan_folder(folder):
-    filenames=os.listdir(f'./{folder}')
-    text=[]
-    for file in filenames:
-        curr_path=f"./{folder}/{file}"
-        if os.path.isfile(curr_path):
-            if file.endswith(('.pdf','.txt','.doc','.docx','.epub','.odt','.pptx')):
-                print(f"Processing file: {curr_path}")
-                text.append(chunk_text(curr_path,500,50))
-
-        if os.path.isdir(curr_path):
-            for dirpath,dirnames,files_in_subdir in os.walk(curr_path):
-                for f_name in files_in_subdir:
-                    if f_name.endswith(('.pdf','.txt','.doc','.docx','.epub','.odt','.pptx')):
-                        full_file_path=os.path.join(dirpath, f_name)
-                        print(f"Processing file in dir: {full_file_path}")
-                        text.append(chunk_text(full_file_path,500,50))
-    return text
-    
-def scan_and_upload(folder):
-    """Groups the folder scan and embedding upload in a single pipeline"""
-    text=scan_folder(folder)
-    print('connecting to mongo...')
-    client=_mongo_client()
-    db=client['vector-db']
-    embeddings_coll=db['embeddings']
-    for chunks in text:
-        embed_and_upload_file(chunks,embeddings_coll)
-
-
-
-# if __name__=='__main__':
-#     mcp.run(
-#         transport="streamable-http",
-#         port=8080,
-#         host="127.0.0.1",
-#         log_level="INFO"
-#     )
+if __name__=='__main__':
+    mcp.run(
+        transport="streamable-http",
+        port=8080,
+        host="127.0.0.1",
+        log_level="INFO"
+    )
