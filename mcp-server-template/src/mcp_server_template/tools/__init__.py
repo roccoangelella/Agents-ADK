ed171fd246d661a3b7ae7173b716ecc3ada86ebb
Rocco Angelella <rocchissimo936@gmail.com>
2025-10-29 16:45:43 +0100

    update .gitignore

--- mcp-server-template/src/mcp_server_template/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/mcp_server_template/tools/__init__.py
-from . import core_tools, quest_tools, utility_tools
-
-# --- IMPORT DEI NUOVI MODULI ---
-from . import stateful_tools
-from . import interactive_tools
-from . import advanced_tools
-from . import image_tools
